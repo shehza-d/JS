f867ebaecfc39d484bf03d6bc4d48a90e5f59702
Shehzad <shehzaddiqbal@gmail.com>
2022-05-16 05:18:49 -0700

    chapter 3 done

---
+++ color.js
@@ -0,0 +1,30 @@
+
+alert("Hello, Assalam o Alaikum");
+alert("Assalam o Alaikum Shehzad");
+alert("Assalam o Alaikum Shehzad");
+console.log("Testing consoleeee");
+let s = 5;
+console.log("s is equal to: ", s);
+
+s = 7;  
+/*  purana wala s wo bhool jae ga */
+console.log("s is equal to: ", s);
+
+let plas = s + 2;
+console.log("total of numbers is :  "  ,  plas);
+
+s = "shehzad";
+console.log("s is equal to: ", s);
+
+
+s = s + 313;
+console.log("s is equal to: ", s);
+
+
+s = s + 24 + 3;
+console.log("s is equal to: ", s);
+
+s = s + (24 + 3)  ;
+console.log("s is equal to: ", s);
+
+let billAmount
