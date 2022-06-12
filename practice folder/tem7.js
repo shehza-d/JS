e5a0c5c97620a879a2c7a227c42a8bc683050034
Shehzad <shehzaddiqbal@gmail.com>
2022-06-12 03:58:26 -0700

    updated

---
+++ practice folder/tem7.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < row; i++) {
+	for (let j = 0; j < column; j++) {
+		let value1 = Number(document.querySelector(`#matrix_value_${i}${j}`)).value;
+		let value2 = Number(document.querySelector(`#matrix_value2_${i}${j}`)).value;
+		let result = value1 + value2;
+		target_result_div.innerHTML += `<td>${result}</td>`;
+	}
+	target_result_div.innerHTML += "<br>";
+}
