24c8fae886afbb268718f0a4c301a892c64fc105
shehzad <shehzaddiqbal@gmail.com>
2022-06-29 23:47:08 +0500

    pushh

--- practice folder/notes.js
+++ practice folder/notes.js
@@ -8,6 +8,16 @@ purity of Function = jis ka output apne input per depend kar raha ho aur predict
 benift of Purity of Function
  Function should be usable as function can be used in react native, react, and MERN and many other places
  jab ak event fire hota hy tw koi event listener ko pata chalta hy ky koi specific kam karna hy
+ 
+ 29-jun-2022
+make a game and time tracker app with JavaScript
+how to convert a website from dark mode to light mode with user's perfrence
+how to create desktop app with JavaScript
+
+
+
+
+
  */
 
 //es5 for HOISTING (named function)
