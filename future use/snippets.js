413b2895eb86a2496c1559ca4aaa2d7280546bf1
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-07-15 00:18:36 +0500

    snippet syntax

---
+++ future use/snippets.js
@@ -0,0 +1,32 @@
+//  https://code.visualstudio.com/docs/editor/userdefinedsnippets#:~:text=VS%20Code%20has%20built%2Din,language%20of%20the%20current%20file.
+//  command pallet ma insert snippet lekho
+document.getElementById('id');
+console.log();
+console.log();
+debugger;
+document.addEventListener('kdf', function (e) {
+    sheh
+}); 
+
+document.querySelector('#para');  //qs
+document.addEventListener('load', function (e) {  //ae
+    // body
+}); 
+  for (let index = 0; index < array.length; index++) { //for
+    const element = array[index];
+    
+  } 
+  switch (key) {  //switch
+    case 1:
+        console.log("sehhzad");         
+
+        break;
+  
+    default:
+        break;
+  }
+
+//   const name = new type(arguments);
+JSON.parse(obj);  //jp
+JSON.stringify(obj); //js
+
