a4b1b4dca469162f6bdab1593bb396062844eff9
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-07-15 01:21:33 +0500

    user manual for js snippets

--- future use/snippets.js
+++ future use/snippets.js
@@ -1,32 +1,23 @@
 //  https://code.visualstudio.com/docs/editor/userdefinedsnippets#:~:text=VS%20Code%20has%20built%2Din,language%20of%20the%20current%20file.
 //  command pallet ma insert snippet lekho
-document.getElementById('id');
-console.log();
-console.log();
-debugger;
-document.addEventListener('kdf', function (e) {
-    sheh
-}); 
 
-document.querySelector('#para');  //qs
-document.addEventListener('load', function (e) {  //ae
-    // body
-}); 
-  for (let index = 0; index < array.length; index++) { //for
-    const element = array[index];
-    
-  } 
-  switch (key) {  //switch
-    case 1:
-        console.log("sehhzad");         
+// fun    		for arrow function
+//if and ifelse for ...
+//cl            for console.log();
+//ae            for document.addEventListener('load', function (e) { body });
+//gi            for document.getElementById('id');
+//qs            for document.querySelector('selector');
+//for           for for (let infor for dex = 0; index < arrayfor for .length; index++) {const elementfor for  = array[index];}
+//foreach       for array.forEach(element => { });
+// switch       for switch (key) {case value:   break;      default:		break;}
+// ra           for document.removeAttribute('attr');
+// ih           for document.innerHTML = 'elem';
 
-        break;
-  
-    default:
-        break;
-  }
 
-//   const name = new type(arguments);
-JSON.parse(obj);  //jp
-JSON.stringify(obj); //js
 
+
+
+fun for costum made es6 arrow function
+let funciton_ka_nam = (arguments)  => {
+//Body;
+} 
