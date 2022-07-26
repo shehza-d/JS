e16ce5dc0d42f9bbd1497c68c9d01aaba2f2ed18
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-07-26 21:22:07 +0500

    dark mode media querry

--- future use/dark-mode.js
+++ future use/dark-mode.js
@@ -5,6 +5,11 @@
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+har mode ky liye alag CSS file banalo simple
+	<link rel="stylesheet" href="./src/lightmode.css" media="(prefers-color-scheme: light)">
+	<link rel="stylesheet" href="./src/darkmode.css" media="(prefers-color-scheme: dark)">
+
 	<title>Document</title>
 	<style>
 		body {
