d44e7bdfc067fa16e7ac772447eb444d36e61934
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-07-26 21:02:09 +0500

    push

--- future use/location.js
+++ future use/location.js
@@ -1,15 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Location</title>
-</head>
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+// 	<meta charset="UTF-8">
+// 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+// 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+// 	<title>Location</title>
+// </head>
 
-<body>
-	<h1>Location</h1>
-	<script>
+// <body>
+// 	<h1>Location</h1>
+// 	<script>
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(showPosition, showError);
 		} else {
@@ -37,7 +37,7 @@
 					break;
 			}
 		}
-	</script>
-</body>
+// 	 </script>
+// </body>
 
-</html>
+// </html> 
