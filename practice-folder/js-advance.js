e494b3ff752f1afbf6b8b3a7da99e4dab08997ec
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-08-28 19:43:05 +0500

    practice

---
+++ practice-folder/js-advance.js
@@ -0,0 +1,11 @@
+"use strict";
+a = 'this is'
+{a = "something else"}
+console.log(a)
+
+///////////////////////////////////////
+const arr = ['t','a','b','c','d','e','f'];
+console.log(arr.length)
+for (let i = 0; i < arr.length; i++) {
+	console.log(i)
+}
