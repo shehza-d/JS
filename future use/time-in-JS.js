e06ed18cf4d7836d056146931e924d004f2e37c8
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-09 16:26:26 +0500

    calculating Execution Time in JS

---
+++ future use/time-in-JS.js
@@ -0,0 +1,24 @@
+const { JSDOM } = require("jsdom");
+const { window } = new JSDOM();
+
+var start = window.performance.now();
+
+// task starts
+for (var i = 0; i < 100000000; i++);
+// task ends
+
+var end = window.performance.now();
+console.log(`Execution time: ${end - start} ms`);
+
+
+
+//this is running in NODE also
+console.time('Execution Time');
+ 
+// task starts
+for (var i = 0; i < 10000000;i++);
+// task ends
+ 
+console.timeEnd('Execution Time');
+
+
