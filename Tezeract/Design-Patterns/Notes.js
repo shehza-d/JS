9f87e30c02059e85ae937e38751d8bb4ca7723c9
Shehzad <shehzaddiqbal@gmail.com>
2023-01-04 12:30:41 +0500

    Design Patterns!

---
+++ Tezeract/Design-Patterns/Notes.js
@@ -0,0 +1,25 @@
+//Design Patterns
+// A re-usable solution that can be applied to occurring problem in software design
+// can also be though of as programming templates
+// Situation vary significantly
+
+// Types of Design Patterns
+// Module
+// Revealing module pattern
+// singleton
+// factory
+// observer
+// mediator
+// state
+
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
