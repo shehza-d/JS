e73c90b5ec80363d0a124061bd52292cb5bb144d
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-04-01 09:15:20 +0500

    push

--- practice-folder/if-ture.js
+++ practice-folder/if-ture.js
@@ -1,28 +1,112 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
-	<script>
-let city = false;
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+// 	<meta charset="UTF-8">
+// 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+// 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+// 	<title>Document</title>
+// </head>
+// <body>
+// <script>
+//</script>
 
-if(city){
-   console.log("true")
-}else{
-console.log("false")
+let me = "shehzad";
+// console.log(me);
+// me
+
+let person = {
+  userName: "Shehzad",
+  age: 19,
+};
+
+// console.log(person);
+
+let city = false;
+// if (city) {
+//   console.log("this is running for true");
+// } else {
+//   console.log("this is running for false");
+// }
+
+// if (1 == "1") {} //ture
+// // if(1=1)     {}//error
+// if (1 === "1") {} //false
+
+/* Conditional (Ternary) Operator */
+// variablename = condition ? value1 : value2;
+
+function sum(x, y) {
+  return x + y;
 }
 
-// if(1=="01") //ture
-// if(1=1)     //error
-// if(1==="1") //false
+// console.log(sum(3, 7));
 
-{/* Conditional (Ternary) Operator */}
+const arr = ["Hi", "how", "are", "you!"];
 
-variablename = (condition) ? value1:value2 
-	</script>
-</body>
-</html>
+// console.log(arr[0]);
+// console.log(arr.pop());
+// console.log(arr.join(" "));
+// for last value
+
+// Simple Array Sum //EasyProblem Solving (Basic)Max Score: 10Success Rate: 94.36%
+
+const arr2 = [2, 4, 2, 9, 6, 5, 9];
+let total = 0;
+arr2.map((i) => (total += i));
+console.log(total);
+
+// console.log(4 % 6);
+
+// HackerRank FizzBuzz problem solved
+// console.log(fizzBuzz(15));
+// fizzBuzz(15);
+
+// Logging value
+function fizzBuzz(n) {
+  // Write your code here
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+    else if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
+    else console.log(i);
+  }
+}
+
+//returning value
+function fizzBuzz(n) {
+  let result = "";
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result += "FizzBuzz\n";
+      continue;
+    } else if (i % 3 === 0) {
+      result += "Fizz\n";
+      continue;
+    } else if (i % 5 === 0) {
+      result += "Buzz\n";
+      continue;
+    } else {
+      result += `${i}\n`;
+    }
+  }
+  return result;
+}
+
+// const arr3 = [3, "foo", "bar", "baz", "shehzad"];
+// // console.log(possibleChanges(arr3));
+// possibleChanges(arr3);
+
+function possibleChanges(usernames) {
+  // Write your code here
+  for (let i = 1; i < usernames.length; i++) {
+    for (let j = 0; j < usernames[i].length; j++) {
+      // if (strcmp(str[i], str[j]) > 0) {      }
+      // if (usernames[1 - i][1 - j] < usernames[i][j]) console.log("success");
+      // if (usernames[i][i] > usernames[i][j]) console.log("ture");
+      // else console.log("flsasese");
+      console.log(usernames[i][j], "i=", i, "j=", j);
+    }
+    console.log("\n");
+  }
+}
--- practice-folder/js-advance.js
+++ practice-folder/js-advance.js
@@ -1,33 +1,35 @@
 "use strict";
-a = 'this is'
-{ a = "something else" }
-console.log(a)
+
+// let a = "this is";
+// {
+//   b = "something else";
+// }
+// console.log(a);
 
 ///////////////////////////////////////
 
-const arr = ['t', 'a', 'b', 'c', 'd', 'e', 'f'];
-console.log(arr.length)
+const arr = ["t", "a", "b", "c", "d", "e", "f"];
+console.log(arr.length);
 for (let i = 0; i < arr.length; i++) {
-	console.log(i)
+  console.log(i);
 }
 
 /////////////////////////////////////////
 // Solution of  https://leetcode.com/problems/two-sum/
 
-const nums = [2, 5, 5, 11]
+const nums = [2, 5, 5, 11];
 let target = 10;
 const twoSum = function (nums, target) {
-
-	for (let i = 0; i < nums.length; i++) {
-		for (let j = 0; j < nums.length; j++) {
-			if (i!=j) {
-				if (nums[i] + nums[j] == target) {
-					console.log(i, j)
-					return;
-				}
-			}
-		}
-	}
-}
-let a = twoSum(nums, target)
-console.log(a)
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i != j) {
+        if (nums[i] + nums[j] == target) {
+          console.log(i, j);
+          return;
+        }
+      }
+    }
+  }
+};
+let a = twoSum(nums, target);
+console.log(a);
